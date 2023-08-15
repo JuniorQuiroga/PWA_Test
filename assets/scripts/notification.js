cb60421150deeee495f44377a173dad4afc2c9d9
JuniorQuiroga <quirogajunior25@gmail.com>
2023-08-14 21:39:42 -0300

    first test

---
+++ assets/scripts/notification.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const permissionBtn = document.getElementById('permissionBtn');
+    const showNotificationBtn = document.getElementById('showNotificationBtn');
+    const notificationMessageInput = document.getElementById('notificationMessage');
+
+    permissionBtn.addEventListener('click', () => {
+        Notification.requestPermission().then(permission => {
+            if (permission === 'granted') {
+                console.log('Notification permission granted.');
+            } else {
+                console.log('Notification permission denied.');
+            }
+        });
+    });
+
+    showNotificationBtn.addEventListener('click', () => {
+        const message = notificationMessageInput.value;
+
+        if (Notification.permission === 'granted' && message) {
+            navigator.serviceWorker.ready.then(registration => {
+                registration.showNotification('Custom Notification', {
+                    body: message,
+                    icon: 'icon.png',
+                    badge: 'icon.png'
+                });
+            });
+        }
+    });
+});
