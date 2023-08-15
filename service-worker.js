3b218af3539f267cf0a02b15a61212f289bd2e59
JuniorQuiroga <quirogajunior25@gmail.com>
2023-08-14 23:06:52 -0300

    push notif plz work

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 self.addEventListener('push', event => {
   const options = {
-      body: event.data.text(),
-      icon: '/assets/images/icon.png',
-      badge: '/assets/images/icon.png'
+    body: event.data.text(),
+    icon: './assets/images/icon.png',
+    badge: './assets/images/icon.png'
   };
-  
+
   event.waitUntil(
-      self.registration.showNotification('Push Notification', options)
+    self.registration.showNotification('Push Notification', options)
   );
 });
